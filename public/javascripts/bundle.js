62bcdd8707fa6986be8479f2b0db151aef18f839
Elijah <ejoleg@gmail.com>
2015-07-18 18:35:58 -0700

    saved fixed firebase

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -3,17 +3,14 @@ var app = angular.module('clarityApp', ['ui.router']);
 app.run(function($rootScope) {
   console.log('clarity app');
 });
-app.controller('loginCtrl', function($scope, $rootScope, $http, urls) {
-	var ref = new Firebase("https://class-clarity.firebaseio.com");
-	$scope.login = function() {
-		ref.authWithOAuthRedirect("google", function(error) {
-			if (error) {
-				console.log("Login Failed!", error);
-			} else {
-				// We'll never get here, as the page will redirect on success.
-			}
-		});
-	};
+
+var ref = new Firebase("https://clarity-control.firebaseio.com");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
 });
 
 app
---
+++ source/javascripts/auth/auth.js
@@ -0,0 +1,8 @@
+var ref = new Firebase("https://clarity-control.firebaseio.com");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
